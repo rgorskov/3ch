86bef0f1abff6537d22b07b6ff8b2ec9ba789d40
rgorskov <rgorskov@gmail.com>
2021-06-27 17:21:03 +0500

    formating date

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import s from './Post.module.css';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const Post = ({ op, date, text, id, threadId, showLink }) => {
     return (
         <div className={s.wrapper}>
             <div className={op ? s.op : s.reply}>
                 <div className={s.info}>
-                    {date.toString()}
+                    {moment(date).format('DD.MM.YYYY ddd HH:mm:ss')}
                     {showLink && <Link to={'/thread/' + threadId}>В тред</Link>}
                 </div>
                 <div className={s.text}>{text}</div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { render } from 'react-dom';
 import App from './App';
 import './index.css';
 import * as api from './api';
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import moment from 'moment';
+
+moment.locale('ru');
 
 window.api = api;
 

2ba10cf34b020653dc1c9475474749c393c00fc8
rgorskov <rgorskov@gmail.com>
2021-07-02 00:52:40 +0500

    ThreadsList: show threads from store

--- src/components/ThreadsList.jsx
+++ src/components/ThreadsList.jsx
@@ -1,50 +1,54 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { createSelector } from 'reselect';
 import { getThreads } from '../data/actions';
 import Post from './Post';
 import s from './ThreadsList.module.css';
 
 const ThreadsList = () => {
-    const isLoaded = useSelector((state) => state.loaded);
     const dispatch = useDispatch();
+    const threadsSelector = createSelector(
+        (state) => state.threads,
+        (threads) =>
+            threads.map((t) => {
+                const threadId = t.id;
+                const opPost = t.posts.find((p) => p.op);
+                const createTime = new Date(opPost.createTime);
+                return {
+                    ...opPost,
+                    createTime,
+                    threadId,
+                };
+            })
+    );
+    const opPosts = useSelector(threadsSelector);
+    const isLoaded = useSelector((state) => state.loaded);
+
     if (!isLoaded) {
         dispatch(getThreads());
+
+        return <div>Place loader here</div>;
     }
 
     return (
         <div>
             <Link to="/thread/create">Создать тред</Link>
-            <div className={s.thread}>
-                <Post
-                    op={true}
-                    text={'OP post'}
-                    id={1}
-                    date={new Date()}
-                    showLink
-                    threadId={1}
-                />
-            </div>
-            <div className={s.thread}>
-                <Post
-                    op={true}
-                    text={'OP post'}
-                    id={1}
-                    date={new Date()}
-                    showLink
-                    threadId={1}
-                />
-            </div>
-            <div className={s.thread}>
-                <Post
-                    op={true}
-                    text={'OP post'}
-                    id={1}
-                    date={new Date()}
-                    showLink
-                    threadId={1}
-                />
-            </div>
+
+            {opPosts.map((p) => {
+                return (
+                    <div className={s.thread} key={p.id}>
+                        <Post
+                            op={true}
+                            text={p.text}
+                            id={p.id}
+                            date={p.createTime}
+                            showLink
+                            threadId={p.threadId}
+                        />
+                    </div>
+                );
+            })}
         </div>
     );
 };
